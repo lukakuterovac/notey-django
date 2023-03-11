02a11fb5374328a060abbb8eddca7e3daf7bcdd6
Luka Kuterovac <luka.kuterovac@gmail.com>
2023-03-11 13:02:58 +0100

    Add login, logout and register functionality
    Remove redundant tag

--- notey/app/urls.py
+++ notey/app/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = "app"
 urlpatterns = [
     path("", views.home, name="home"),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout"),
 ]
